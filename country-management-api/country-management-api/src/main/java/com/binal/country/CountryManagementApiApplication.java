cc828d54ea27ff7f0b2aa4252c7c961011f43648
Sumit-Rajak <sumitbhairajak@gmail.com>
2026-02-04 00:36:24 +0530

    feign added

--- country-management-api/country-management-api/src/main/java/com/binal/country/CountryManagementApiApplication.java
+++ country-management-api/country-management-api/src/main/java/com/binal/country/CountryManagementApiApplication.java
@@ -2,8 +2,10 @@ package com.binal.country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.binal.country.feignclient")
 public class CountryManagementApiApplication {
 
 	public static void main(String[] args) {
